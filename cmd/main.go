700cc97bd9069d8fe3b52d1282959326e3f3b970
LEONARDO GIUSEPPE DE SOUZA RAFAELLI <leonardo_rafaelli@estudante.sesisenai.org.br>
2022-10-07 21:53:38 -0300

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	login()
+}
+
+func login() {
+
+	fmt.Println(4 >> 2)
+	defer deferTry()
+
+	fmt.Println("Chegou aqui")
+	fmt.Println("Chegou aqui X2")
+}
+
+func deferTry() {
+	fmt.Println("Chegou X3")
+}
---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,6 @@
+package routes;
+
+// import (
+	// "github.com/gorilla/mux"
+	// "github.com/leonardorafaelli/go-bookstore/pkg/controllers"
+// )
