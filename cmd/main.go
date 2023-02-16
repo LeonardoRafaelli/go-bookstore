68f4bb0120eff094274e64c6bbf1520f9387d4d1
leonardorafaelli <leonardorafaelli@gmail.com>
2023-02-16 01:34:03 -0300

    refactor: delete useless import in pkg/cmd

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
 	"github.com/LeonardoRafaelli/go-bookstore/pkg/routes"
 	"github.com/gorilla/mux"
+	"log"
+	"net/http"
 )
 
 func main() {
-	r := mux.NewRouter();
-	routes.RegisterBookStoreRoutes(r);
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
 	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }
-
